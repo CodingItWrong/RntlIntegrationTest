e66298977e3008375fcf16f2ed30b6eed0bda76a
Josh Justice <josh.justice@accesscfa.com>
2021-03-04 08:33:29 -0500

    Install RNav

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaView, StatusBar, Text} from 'react-native';
 
 export default function App() {
   return (
-    <>
+    <NavigationContainer>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <Text>Hello, React Native!</Text>
       </SafeAreaView>
-    </>
+    </NavigationContainer>
   );
 }
