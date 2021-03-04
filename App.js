a028cf6006488a7c8ed8b3b4bdd9add6859296b7
Josh Justice <josh.justice@accesscfa.com>
2021-03-04 08:34:42 -0500

    Show stack nav

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {SafeAreaView, StatusBar, Text} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Text, View} from 'react-native';
+
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <Text>Hello, React Native!</Text>
-      </SafeAreaView>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
