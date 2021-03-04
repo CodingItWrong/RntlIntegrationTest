423e66ab25ea7999f1b1b80468ebfe712e4f9bb7
Josh Justice <josh.justice@accesscfa.com>
2021-03-04 09:11:28 -0500

    Test click

--- __tests__/HomeScreen.spec.js
+++ __tests__/HomeScreen.spec.js
@@ -1,6 +1,6 @@
 import 'react-native';
 import React from 'react';
-import {render} from '@testing-library/react-native';
+import {render, fireEvent} from '@testing-library/react-native';
 import HomeScreen from '../HomeScreen';
 
 describe('HomeScreen', () => {
@@ -9,5 +9,12 @@ describe('HomeScreen', () => {
     expect(queryByText('Home Screen')).not.toBeNull();
   });
 
-  it.todo('allows navigating');
+  it('allows navigating', () => {
+    const navigation = {
+      navigate: jest.fn().mockName('navigation.navigate'),
+    };
+    const {getByText} = render(<HomeScreen navigation={navigation} />);
+    fireEvent.press(getByText('Go to Details'));
+    expect(navigation.navigate).toHaveBeenCalled();
+  });
 });
